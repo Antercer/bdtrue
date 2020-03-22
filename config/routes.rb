509f0c49e09bbee26c3d2b8579c435ce18531713
Antercer <alonsomancini@gmail.com>
2020-03-22 14:09:35 -0300

    before posts as objects

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get '/home', to: 'home#home'
   get '/secciones', to: 'secciones#index'
   get '/posts', to: 'posts#show'
   resources :posts
+  resources :secciones
 end
