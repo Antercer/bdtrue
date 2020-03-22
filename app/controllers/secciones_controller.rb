ce26f928bd74f4c3974e47969e6cf2890d4caf08
Antercer <alonsomancini@gmail.com>
2020-03-22 15:48:50 -0300

    pre seccions rename

--- app/controllers/secciones_controller.rb
+++ app/controllers/secciones_controller.rb
@@ -1,11 +1,11 @@
 class SeccionesController < ApplicationController
     def index
-        @board_name = params[:board]
-        #@board = Board.where(name:@board_name)[0]
-        @posts = Post.all#.where(board:@board)
+       @secciones = Seccion.all
     end
-    #def show
-       # @post = Post.find(params[:id])
-    #end
+
+    def show
+        @seccion = Seccion.find(params[:id])
+        @posts = Post.all
+    end
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get '/home', to: 'home#home'
-  get '/secciones', to: 'secciones#index'
+  get '/secciones', to: 'secciones#show'
+  get '/index', to: 'secciones#index'
   get '/posts', to: 'posts#show'
   resources :posts
   resources :secciones
